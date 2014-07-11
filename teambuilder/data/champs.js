85a9d4cc09c7a0c5bdbcf586f348c4633d171438
JsKingBoo <JsKingBoo@Gmail.com>
2014-07-11 07:43:18 -0400

    Add placeholder files for pages and start adding raw champ data

--- teambuilder/data/champs.js
+++
@@ -1,8 +0,0 @@
-//id, number, display name, blurb, type, base stats (HP, AD, armor, AP, MR, armor pen, m pen, mana, speed), passive, 2nd form or N/A
-exports.Champions = {
-soraka:{num:1,display:"Soraka",blurb:"The Starchild",types:["Fairy"],baseStats:{hp:405,ad:48,arm:17,ap:50,mr:30,ape:0,mpe:15,man:240,spe:340},passive:"Salvation"},
-teemo:{num:2,display:"Teemo",blurb:"The Swift Scout",types:["Dark", "Poison"],baseStats:{hp:375,ad:44,arm:18,ap:40,mr:30,ape:0,mpe:20,man:200,spe:330},passive:"Camouflage"},
-sivir:{num:3,display:"Sivir",blurb:"The Battle Mistress",types:["Fighting"],baseStats:{hp:378,ad:60,arm:15,ap:20,mr:30,ape:20,mpe:0,man:200,spe:335},passive:"Fleet of Foot"},
-sion:{num:4,display:"Sion",blurb:"The Undead Champion",types:["Fighting", "Ghost"],baseStats:{hp:403,ad:55,arm:22,ap:50,mr:35,ape:0,mpe:0,man:200,spe:345},passive:"Feel No Pain"},
-
-}
---
+++ template/navbar.js
@@ -0,0 +1,68 @@
+function drawNavbar(active){ //"home", "teambuilder", "play", "info"
+
+	var html = '';
+	html =  '<nav class="navbar navbar-default" role="navigation">';
+	html += '  <div class="container-fluid">';
+	html += '  <!-- Brand and toggle get grouped for better mobile display -->';
+	html += '    <div class="navbar-header">';
+	html += '      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">';
+	html += '        <span class="sr-only">Toggle navigation</span>';
+	html += '        <span class="icon-bar"></span>';
+	html += '        <span class="icon-bar"></span>';
+	html += '        <span class="icon-bar"></span>';
+	html += '      </button>';
+	html += '      <a class="navbar-brand" href="#">Brand</a>';
+	html += '    </div>';
+	html += '    <!-- Collect the nav links, forms, and other content for toggling -->';
+	html += '    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">';
+	html += '      <ul class="nav navbar-nav">';
+	
+	if (active == 'home'){ //check for active home
+	  html += '        <li class="active"><a href="#">Home</a></li>';	
+	} else {
+	  html += '        <li><a href="../home/index.html">Home</a></li>';	
+	}
+	
+	if (active == 'teambuilder'){ //check for active teambuilder
+	  html += '        <li class="active"><a href="#">Teambuilder</a></li>';		
+	} else {
+	  html += '        <li><a href="../teambuilder/main.html">Teambuilder</a></li>';	
+	}
+
+	if (active == 'play'){
+	  html += '        <li class="active"><a href="#">Play</a></li>';		
+	} else {
+	  html += '        <li><a href="../play/main.html">Play</a></li>';	
+	}
+	
+	//dropdown
+	
+	html += '        <li class="dropdown">';
+	html += '          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Information and Leagues<span class="caret"></span></a>';
+	html += '          <ul class="dropdown-menu" role="menu">';
+	html += '            <li><a href="#">Champion Info</a></li>';
+	html += '            <li><a href="#">Match Mechanics Info</a></li>';
+	html += '            <li class="divider"></li>';
+	html += '            <li><a href="#">Leaderboards</a></li>'
+	html += '            <li><a href="#">Winrates</a></li>'
+	html += '            <li class="divider"></li>';
+	html += '            <li><a href="#">Contact</a></li>';
+	html += '            <li class="divider"></li>'
+	html += '            <li><a href="http://na.leagueoflegends.com/">League of Legends website</a></li>';
+	html += '            <li><a href="http://pokemonshowdown.com/">Pokemon simulator</a></li>';		
+	html += '          </ul>';
+	html += '        </li>';
+	html += '      </ul>';
+	html += '      <form class="navbar-form navbar-left" role="search">';
+	html += '        <div class="form-group">';
+	html += '          <input type="text" class="form-control" placeholder="Search">';
+	html += '        </div>';
+	html += '        <button type="submit" class="btn btn-default">Submit</button>';
+	html += '      </form>';
+	html += '    </div>     <!-- /.navbar-collapse -->';
+	html += '  </div>       <!-- /.container-fluid -->';
+	html += '</nav>';
+
+	document.write(html);
+
+}
