c979bf01462fffeddd04ad3b9216473dfc330269
JsKingBoo <JsKingBoo@Gmail.com>
2014-07-24 07:37:51 -0400

    Call data from exports test

---
+++ data/display.js
@@ -0,0 +1,10 @@
+function display(active){ //"home", "teambuilder", "play", "info"
+
+	var displayContainer = document.getElementById("display-holder");
+
+	var html = '';
+
+
+	displayContainer.innerHTML=html;
+
+}
