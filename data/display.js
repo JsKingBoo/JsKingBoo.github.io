4ed17a7d3b1fe07c40a00ac52543a812b2f7f536
JsKingBoo <JsKingBoo@Gmail.com>
2014-07-26 07:13:05 -0400

    Add function to buttons on info page

--- data/display.js
+++ data/display.js
@@ -1,7 +1,7 @@
-function displayInfo(){
+var displayContainer = document.getElementById("display-holder");
+var html = '';
 
-	var displayContainer = document.getElementById("display-holder");
-	var html = '';
+function displayInfo(){
 	
 	homeScreen();
 	
@@ -9,18 +9,52 @@ function displayInfo(){
 	
 		var displayArray = [];
 	
-		for(var j in exports.champs) {
-			//champsArray[j]	
+		for(var j in exports.champs) { //arrange display names in alpha order	
 			displayArray.push(exports['champs'][j]['display'])
-			//html += '<button href="#" class="btn btn-primary" style="margin-left:40px; width:1200px; height:120px;">' + exports['champs'][j]['display'] + '</button>';
 		}
 		displayArray.sort();
-		for(var j in displayArray) {
-			html += '<button href="#" id="' + displayArray[j] + '" class="btn btn-primary" style="margin-left:40px; width:1200px; height:120px;">' + displayArray[j] + '</button>';
+		for(var j in displayArray) { //get code names
+			var id = '';
+			
+			for(var k in exports.champs) {
+				if(exports['champs'][k]['display'] == displayArray[j]) {
+					id = k;
+				}
+			}
+
+			html += '<button href="#" id="' + id + '" onclick="onPress(&quot;' + id + '&quot;)" class="btn btn-primary" style="margin-left:40px; width:1200px; height:120px;">' + displayArray[j] + '</button>';
 		}
-	
+		
+		displayContainer.innerHTML=html;	//this = DONE WITH FUNCTION	
+
 	}
 	
-	displayContainer.innerHTML=html;
+
+	
 
 }
+
+function onPress(chp){ //champ code name, NOT display name
+
+	var PROFILE_WIDTH = 500; //all caps = constant. fact
+	var OFFSET = 40;
+	var SCREENWIDTH = screen.width;
+
+	html = '';
+
+	html += '<div class="row">';
+	html += '	<div class="col-xs-6 col-md-3" style="margin-left:40px;">';
+	html += '		<a href="#" class="thumbnail">';
+	html += '			<img src="http://fc02.deviantart.net/fs70/f/2012/289/3/c/pin_template_by_neooki23-d5hzqob.png" class="img-polaroid" alt="...">';
+	html += '		</a>';
+	html += '	</div>';
+	html += '</div>';
+	//image is 500x500
+	html += '<div id="title">';
+	html += '	<h1 style="position:relative;top:-290px;left:400px;font-size:48pt">' + exports['champs'][chp]['display'] + '</h1>';
+	html += '	<h2 style="position:relative;top:-270px;left:400px">'+ exports['champs'][chp]['blurb'] +'</h2>';
+	html += '</div>';
+	
+	displayContainer.innerHTML=html; //DONE WITH FUNCTION
+}	
+
