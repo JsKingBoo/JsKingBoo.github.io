313d33bf5e1ffe19cad659d44328e5ce2123cdc8
JsKingBoo <JsKingBoo@Gmail.com>
2014-07-29 07:21:03 -0400

    Added Sion

--- data/moves.js
+++ data/moves.js
@@ -8,7 +8,7 @@ category: "Physical", "Magic", "True", "Status"
 cooldown is counted in turns
 cost is of any resource
 priority: default 0, bigger number = go faster, same number = whoever's speed is faster
-target: "self", "enemy", "ally", "allEnemies" (aoe), "allAllies" (aoe), "allyOrSelf", "any", "enemyOrAlly"
+target: "self", "enemy", "ally", "allEnemies" (aoe), "allAllies" (aoe), "allAlliesAndSelf", "allyOrSelf", "enemyOrAlly", "any", "enemySide", "allySide"
 CC: "slow", "blind", "root", "silence", "airborne", "stun", "taunt", "fear", "suppression", "mrshred", "armshred"
 buffs: "movespeed", "ad", "invisibility", "tenacity", "shield"
 type: "normal", "fighting" etc etc
@@ -16,6 +16,7 @@ contact: only for thornmail purposes
 projectile: only for windwall purposed
 */
 moves = {
+	//soraka
 	starcall:{
 		accuracy:100,
 		base:40,
@@ -83,7 +84,286 @@ moves = {
 		type:"Fairy",
 		contact:false,
 		projectile:false,
-		description:""
+		description:"On ally cast, Soraka drains her mana to restore mana to her target ally. Soraka cannot use Infuse on herself. On enemy cast, Soraka silences the target and deals magic damage to them."
 	},
-
+	wish:{
+		accuracy:true,
+		base:150,
+		scaling:{
+			AP:55
+		},
+		dependent:{},
+		category:"Status",
+		display:"Wish",
+		cooldown:30,
+		cost:100,
+		priority:0,
+		target:"allAlliesAndSelf",
+		CC:{},
+		buffs:{
+			heal:true,
+		},
+		type:"Fairy",
+		contact:false,
+		projectile:false,
+		description:"Soraka fills her allies with hope, restoring health to herself and all friendly champions. Wish can affect untargetable allies."
+	},
+	//teemo
+	blindingdart:{
+		accuracy:100,
+		base:80,
+		scaling:{
+			AP:80
+		},
+		dependent:{},
+		category:"Magic",
+		display:"Blinding Dart",
+		cooldown:8,
+		cost:70,
+		priority:0,
+		target:"enemy",
+		CC:{
+			blind:2
+		},
+		buffs:{},
+		type:"Poison",
+		contact:false,
+		projectile:true,
+		description:"Teemo shoots a dart at an enemy, dealing magic damage and blinding them for 2 turns."
+	},
+	movequick:{
+		accuracy:true,
+		base:0,
+		scaling:{},
+		dependent:{},
+		category:"Status",
+		display:"Move Quick",
+		cooldown:5,
+		cost:40,
+		priority:0,
+		target:"self",
+		CC:{},
+		buffs:{
+			movespeed:1
+		},
+		type:"Dark",
+		contact:false,
+		projectile:false,
+		description:"Teemo gains increased movement speed."
+	},
+	toxicshot:{
+		accuracy:true,
+		base:20,
+		scaling:{
+			AP:40
+			AD:100
+		},
+		dependent:{},
+		category:"Magic",
+		display:"Toxic Shot",
+		cooldown:2,
+		cost:5,
+		priority:0,
+		target:"enemy",
+		CC:{},
+		buffs:{},
+		type:"Poison",
+		contact:false,
+		projectile:true,
+		description:"Teemo's next attack deals bonus magic damage"
+	},	
+	noxioustrap:{
+		accuracy:true,
+		base:0,
+		scaling:{},
+		dependent:{},
+		category:"Magic",
+		display:"Noxious Trap",
+		cooldown:3,
+		cost:85,
+		priority:0,
+		target:"enemySide",
+		CC:{
+			noxiousTrap:1
+		},
+		buffs:{},
+		type:"Poison",
+		contact:false,
+		projectile:false,
+		description:"Teemo places a mushroom trap on the ground. On enemy switch-in, all placed mushrooms explode dealing magic damage."
+	},	
+	//sivir
+	boomerangblade:{
+		accuracy:90,
+		base:40,
+		scaling:{
+			AD:100
+			AP:50
+		},
+		dependent:{
+			multihit:2
+		},
+		category:"Physical",
+		display:"Boomerang Blade",
+		cooldown:5,
+		cost:70,
+		priority:0,
+		target:"allEnemies",
+		CC:{},
+		buffs:{},
+		type:"Normal",
+		contact:false,
+		projectile:true,
+		description:"Sivir hurls her crossblade to a target location, which then returns to her."
+	},		
+	ricochet:{
+		accuracy:100,
+		base:0,
+		scaling:{
+			AD:50
+		},
+		dependent:{},
+		category:"Physical",
+		display:"Ricochet",
+		cooldown:9,
+		cost:60,
+		priority:0,
+		target:"allEnemies",
+		CC:{},
+		buffs:{},
+		type:"Normal",
+		contact:false,
+		projectile:true,
+		description:"Sivir's crossblade bounce to nearby enemies, dealing a percentage of her attack damage to each enemy hit."
+	},
+	spellshield:{
+		accuracy:true,
+		base:80,
+		scaling:{},
+		dependent:{},
+		category:"Status",
+		display:"Spell Shield",
+		cooldown:15,
+		cost:0,
+		priority:0,
+		target:"self",
+		CC:{},
+		buffs:{
+			spellshield:1,
+			manaheal:true		
+		},
+		type:"Psychic",
+		contact:false,
+		projectile:false,
+		description:"Sivir creates a magical barrier that lasts up to 1.5 seconds, blocking the next enemy ability used on Sivir."
+	},
+	onthehunt:{
+		accuracy:true,
+		base:2,
+		scaling:{},
+		dependent:{},
+		category:"Status",
+		display:"On The Hunt",
+		cooldown:30,
+		cost:100,
+		priority:0,
+		target:"allAlliesAndSelf",
+		CC:{},
+		buffs:{
+			movespeed:2		
+		},
+		type:"Normal",
+		contact:false,
+		projectile:false,
+		description:"Sivir rallies her allies, granting all nearby allies and herself a movement speed bonus."
+	},	
+	//sion
+	crypticgaze:{
+		accuracy:100,
+		base:70,
+		scaling:{
+			AP:90
+		},
+		dependent:{},
+		category:"Magic",
+		display:"Cryptic Gaze",
+		cooldown:10,
+		cost:100,
+		priority:0,
+		target:"enemy",
+		CC:{
+			stun:1
+		},
+		buffs:{},
+		type:"Dark",
+		contact:false,
+		projectile:true,
+		description:"Sion's gaze terrifies a single enemy."
+	},
+	deathscaress:{
+		accuracy:true,
+		base:100,
+		scaling:{
+			AP:90
+		},
+		dependent:{},
+		category:"Status",
+		display:"Death's Caress",
+		cooldown:18,
+		cost:100,
+		priority:0,
+		target:"self",
+		CC:{},
+		buffs:{
+			shield:true
+		},
+		type:"Dark",
+		contact:false,
+		projectile:false,
+		description:"Sion surrounds himself with a shield which absorbs damage."
+	},
+	enrage:{
+		accuracy:true,
+		base:25,
+		scaling:{
+			AD:100
+		},
+		dependent:{},
+		category:"Physical",
+		display:"Enrage",
+		cooldown:5,
+		cost:80,
+		priority:0,
+		target:"self",
+		CC:{},
+		buffs:{
+			heal:25
+		},
+		type:"Dark",
+		contact:false,
+		projectile:false,
+		description:"Sion deals bonus damage on his next attack and heals himself."
+	},
+	cannibalism:{
+		accuracy:true,
+		base:50,
+		scaling:{
+			AD:100
+		},
+		dependent:{},
+		category:"Physical",
+		display:"Cannibalism",
+		cooldown:30,
+		cost:90,
+		priority:0,
+		target:"allAlliesAndSelf",
+		CC:{},
+		buffs:{
+			heal:true
+		},
+		type:"Dark",
+		contact:false,
+		projectile:false,
+		description:"Sion deals damage and heals for the amount of damage dealt."
+	},
 }
