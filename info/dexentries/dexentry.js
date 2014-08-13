aad9849492dff590bf2129e0a626cb05570c9d23
JsKingBoo <JsKingBoo@Gmail.com>
2014-08-13 06:18:56 -0400

    Attempt to add movepool

--- info/dexentries/dexentry.js
+++ info/dexentries/dexentry.js
@@ -14,10 +14,27 @@ function champInfo(chp){ //champ code name, NOT display name
 	html += '</div>';
 	//image is 480x480
 	
+	html += '<div id="movepool">';
+	html += '	<div class="btn-group">';
+	html += '		<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">';
+	html += '			Action <span class="caret"></span>';
+	html += '		</button>';
+	html += '		<ul class="dropdown-menu" role="menu">';
+	html += '			<li><a href="#">Action</a></li>';
+	html += '			<li><a href="#">Another action</a></li>';
+	html += '			<li><a href="#">Something else here</a></li>';
+	html += '			<li class="divider"></li>';
+	html += '			<li><a href="#">Separated link</a></li>';
+	html += '		</ul>';
+	html += '	</div>';
+	html += '</div>';	
+	
 	html += '<div id="title">';
 	html += '	<h1 style="position:relative;top:-285px;left:400px;font-size:52pt">' + exports['champs'][chp]['display'] + '</h1>'; //champ name
 	html += '	<h2 style="position:relative;top:-275px;left:400px;font-size:30pt">'+ exports['champs'][chp]['blurb'] +'</h2>'; //blurb
 	html += '</div>';
+	
+
 	
 	displayContainer.innerHTML=html; //DONE WITH FUNCTION	
 	
