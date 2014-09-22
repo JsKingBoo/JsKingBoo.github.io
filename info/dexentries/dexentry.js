89a9b1cbf690579363d9fd28acbdcc4d6893c327
JsKingBoo <JsKingBoo@Gmail.com>
2014-09-22 19:30:19 -0400

    nnnnnnnn

--- info/dexentries/dexentry.js
+++ info/dexentries/dexentry.js
@@ -7,9 +7,9 @@ function champInfo(chp){ //champ code name, NOT display name
 	//splash
 	html += '<div class="container">';
 	html += '	<div class="jumbotron">';
-	html += '		<img src="/data/splashart/' + chp + '.png" class="img-polaroid" alt="" id="splash">'; //all art by Riot Games (tm)
-	html += '		<h1 style="display:inline">' + exports['champs'][chp]['display'] + '</h1>'; //heading
-	html += '		<h2 style="display:inline">' + exports['champs'][chp]['blurb']   + '</h2>'; //blurb
+	html += '		<img style="pull:left" src="/data/splashart/' + chp + '.png" class="img-polaroid" alt="" id="splash" />'; //all art by Riot Games (tm)
+	html += '		<h1>' + exports['champs'][chp]['display'] + '</h1>'; //heading
+	html += '		<h2>' + exports['champs'][chp]['blurb']   + '</h2>'; //blurb
 	html += '	</div>';
 	//image is 480x480
 	//movepools ??
