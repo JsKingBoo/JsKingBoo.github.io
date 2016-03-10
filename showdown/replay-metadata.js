698816dfb98d3d6d8778b81bbfd438cb26a18a29
JsKingBoo <JsKingBoo@Gmail.com>
2016-03-09 20:10:54 -0500

    add replays, add find battler feature

--- showdown/replay-metadata.js
+++ showdown/replay-metadata.js
@@ -1,5 +1,11 @@
 var replayMetadata = [
-{filename:"OU-2015-12-10-zxd354-jskingboo",tier:"OU",date:"2015/12/10",battlers:["Zxd 354", "JsKingBoo"],turns:314,description:"Drawn out stall versus fat balanced team",victor:1},
-{filename:"OU-2016-02-24-jskingboo-zepereira",tier:"OU",date:"2016/02/24",battlers:["JsKingBoo", "Zepereira"],turns:369,description:"Scald stall versus Shedinja stall shenanigans",victor:0},
-{filename:"OU-2016-02-28-ftiger98-jskingboo",tier:"OU",date:"2016/02/28",battlers:["ftiger98", "JsKingBoo"],turns:34,description:"Offensive team's core cannot land a hit due to bad RNG",victor:1}
+{filename:"OU-2015-12-10-zxd354-jskingboo",tier:"OU",date:"2015/12/10",battlers:["Zxd 354", "JsKingBoo"],turns:314,description:"Drawn out stall versus fat balanced team",victor:1,elo:1100},
+{filename:"OU-2016-02-24-jskingboo-zepereira",tier:"OU",date:"2016/02/24",battlers:["JsKingBoo", "Zepereira"],turns:369,description:"Scald stall versus Shedinja stall shenanigans",victor:0,elo:1700},
+{filename:"OU-2016-02-28-ftiger98-jskingboo",tier:"OU",date:"2016/02/28",battlers:["ftiger98", "JsKingBoo"],turns:34,description:"Offensive team's core cannot land a hit due to bad RNG",victor:1,elo:1700},
+{filename:"OU-2016-03-02-ttmvic-azingu",tier:"OU",date:"2016/03/02",battlers:["TTM Vic", "Azingu"],turns:23,description:"VoltTurn team secures a victory...until they outpredict themselves",victor:1,elo:1400},
+{filename:"OU-2016-03-08-starscape-jskingboo",tier:"OU",date:"2016/08/02",battlers:["Starscape", "JsKingBoo"],turns:257,description:"Scald stall versus Shedinja stall shenanigans",victor:1,elo:1800},
+{filename:"OU-2016-03-08-jskingboo-kingbowser",tier:"OU",date:"2016/08/02",battlers:["JsKingBoo", "KingBowser"],turns:58,description:"A demonstration of ensuring perfect counters by Scald stall",victor:0,elo:1700},
+{filename:"OU-2016-03-09-jskingboo-sidefx",tier:"OU",date:"2016/09/02",battlers:["JsKingBoo", "SideFX"],turns:32,description:"Scald stall manages to just barely take down CM Latios and LO Diggersby before cleaning up the rest of the team",victor:0,elo:1750},
+{filename:"OU-2016-03-09-azingu-bigchik",tier:"OU",date:"2016/09/02",battlers:["Azingu", "BigChik"],turns:29,description:"1 HP",victor:0,elo:1300},
+
 ];
