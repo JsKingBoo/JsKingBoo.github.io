6b4d540c6b3adc8c8ced6d634b631dfac941e608
JsKingBoo <JsKingBoo@Gmail.com>
2017-02-13 21:34:10 -0500

    vs mukmuk1 g3

--- showdown/replay-metadata.js
+++ showdown/replay-metadata.js
@@ -1,24 +1,24 @@
 var replayMetadata = [
-{filename:"OU-2015-12-10-zxd354-jskingboo",tier:"OU",date:"2015/12/10",battlers:["Zxd 354", "JsKingBoo"],turns:314,description:"Drawn out stall versus fat balanced team",victor:1,elo:1100},
-{filename:"OU-2016-02-24-jskingboo-zepereira",tier:"OU",date:"2016/02/24",battlers:["JsKingBoo", "Zepereira"],turns:369,description:"Scald stall versus Shedinja stall shenanigans",victor:0,elo:1700},
-{filename:"OU-2016-02-28-ftiger98-jskingboo",tier:"OU",date:"2016/02/28",battlers:["ftiger98", "JsKingBoo"],turns:34,description:"Offensive team's core cannot land a hit due to bad RNG",victor:1,elo:1700},
-{filename:"OU-2016-03-02-ttmvic-azingu",tier:"OU",date:"2016/03/02",battlers:["TTM Vic", "Azingu"],turns:23,description:"VoltTurn team secures a victory...until they outpredict themselves",victor:1,elo:1400},
-{filename:"OU-2016-03-08-starscape-jskingboo",tier:"OU",date:"2016/03/08",battlers:["Starscape", "JsKingBoo"],turns:257,description:"Scald stall versus Shedinja stall shenanigans",victor:1,elo:1800},
-{filename:"OU-2016-03-08-jskingboo-kingbowser",tier:"OU",date:"2016/03/08",battlers:["JsKingBoo", "KingBowser"],turns:58,description:"A demonstration of ensuring perfect counters by Scald stall",victor:0,elo:1700},
-{filename:"OU-2016-03-09-jskingboo-sidefx",tier:"OU",date:"2016/03/09",battlers:["JsKingBoo", "SideFX"],turns:32,description:"Scald stall manages to just barely take down CM Latios and LO Diggersby before cleaning up the rest of the team",victor:0,elo:1750},
-{filename:"OU-2016-03-09-azingu-bigchik",tier:"OU",date:"2016/03/09",battlers:["Azingu", "BigChik"],turns:29,description:"1 HP",victor:0,elo:1300},
-{filename:"OU-2016-03-13-azingu-themichealsuarez",tier:"OU",date:"2016/03/13",battlers:["Azingu", "THE Micheal Suarez"],turns:20,description:"75% chance of a 6-0 sweep",victor:0,elo:1300},
-{filename:"OU-2016-03-14-pancaketherapy-jskingboo",tier:"OU",date:"2016/03/14",battlers:["Pancake Therapy", "JsKingBoo"],turns:111,description:"Scald stall predicts well, but not perfectly, versus a balanced VoltTurn team (top 50 at the time)",victor:1,elo:1800},
-{filename:"OU-2016-03-14-azingu-rea736",tier:"OU",date:"2016/03/14",battlers:["Azingu", "rea736"],turns:24,description:"Outplaying Sceptile's Bisharp, plus a lucky 3-turn Outrage",victor:0,elo:1400},
-{filename:"OU-2016-03-16-azingu-notcute",tier:"OU",date:"2016/03/16",battlers:["Azingu", "Not Cute"],turns:30,description:"Close game between two hyper offensive teams. Also Not Cute is actually a pretty cute guy",victor:0,elo:1500},
+{filename:"OU-2015-12-10-zxd354-jskingboo",tier:"ORAS OU",date:"2015/12/10",battlers:["Zxd 354", "JsKingBoo"],turns:314,description:"Drawn out stall versus fat balanced team",victor:1,elo:1100},
+{filename:"OU-2016-02-24-jskingboo-zepereira",tier:"ORAS OU",date:"2016/02/24",battlers:["JsKingBoo", "Zepereira"],turns:369,description:"Scald stall versus Shedinja stall shenanigans",victor:0,elo:1700},
+{filename:"OU-2016-02-28-ftiger98-jskingboo",tier:"ORAS OU",date:"2016/02/28",battlers:["ftiger98", "JsKingBoo"],turns:34,description:"Offensive team's core cannot land a hit due to bad RNG",victor:1,elo:1700},
+{filename:"OU-2016-03-02-ttmvic-azingu",tier:"ORAS OU",date:"2016/03/02",battlers:["TTM Vic", "Azingu"],turns:23,description:"VoltTurn team secures a victory...until they outpredict themselves",victor:1,elo:1400},
+{filename:"OU-2016-03-08-starscape-jskingboo",tier:"ORAS OU",date:"2016/03/08",battlers:["Starscape", "JsKingBoo"],turns:257,description:"Scald stall versus Shedinja stall shenanigans",victor:1,elo:1800},
+{filename:"OU-2016-03-08-jskingboo-kingbowser",tier:"ORAS OU",date:"2016/03/08",battlers:["JsKingBoo", "KingBowser"],turns:58,description:"A demonstration of ensuring perfect counters by Scald stall",victor:0,elo:1700},
+{filename:"OU-2016-03-09-jskingboo-sidefx",tier:"ORAS OU",date:"2016/03/09",battlers:["JsKingBoo", "SideFX"],turns:32,description:"Scald stall manages to just barely take down CM Latios and LO Diggersby before cleaning up the rest of the team",victor:0,elo:1750},
+{filename:"OU-2016-03-09-azingu-bigchik",tier:"ORAS OU",date:"2016/03/09",battlers:["Azingu", "BigChik"],turns:29,description:"1 HP",victor:0,elo:1300},
+{filename:"OU-2016-03-13-azingu-themichealsuarez",tier:"ORAS OU",date:"2016/03/13",battlers:["Azingu", "THE Micheal Suarez"],turns:20,description:"75% chance of a 6-0 sweep",victor:0,elo:1300},
+{filename:"OU-2016-03-14-pancaketherapy-jskingboo",tier:"ORAS OU",date:"2016/03/14",battlers:["Pancake Therapy", "JsKingBoo"],turns:111,description:"Scald stall predicts well, but not perfectly, versus a balanced VoltTurn team (top 50 at the time)",victor:1,elo:1800},
+{filename:"OU-2016-03-14-azingu-rea736",tier:"ORAS OU",date:"2016/03/14",battlers:["Azingu", "rea736"],turns:24,description:"Outplaying Sceptile's Bisharp, plus a lucky 3-turn Outrage",victor:0,elo:1400},
+{filename:"OU-2016-03-16-azingu-notcute",tier:"ORAS OU",date:"2016/03/16",battlers:["Azingu", "Not Cute"],turns:30,description:"Close game between two hyper offensive teams. Also Not Cute is actually a pretty cute guy",victor:0,elo:1500},
 {filename:"OUsuspecttest-2016-04-04-jskingboo-christou",tier:"OU Suspect Test (Hoopa-U)",date:"2016/04/04",battlers:["JsKingBoo", "christou"],turns:33,description:"Hilarious Talonflame predictions",victor:0,elo:1350},
-{filename:"OU-2016-05-10-notdfficult-jskingboo",tier:"OU",date:"2016/05/10",battlers:["notdfficuLt", "JsKingBoo"],turns:41,description:"Celebrating 1800 Elo featuring a win off hax",victor:1,elo:1800},
-{filename:"OU-2016-05-11-ktutverde-jskingboo",tier:"OU",date:"2016/05/11",battlers:["Ktutverde", "JsKingBoo"],turns:2,description:"JsKingBoo tilts to oblivion shortly after reaching 1800 Elo",victor:0,elo:1700},
-{filename:"OU-2016-05-14-jskingboo-ultair",tier:"OU",date:"2016/05/14",battlers:["JsKingBoo", "Ultair"],turns:8,description:"A \"friendly\" match featuring madness and stupidity",victor:0,elo:1500},
-{filename:"OU-2016-09-21-oumodsrfags-noobab",tier:"OU",date:"2016/09/21",battlers:["OUmodsRfags", "Noobab"],turns:371,description:"Stall versus stall, featuring King Zappy",victor:0,elo:1800},
-{filename:"OU-2016-10-06-abr-idmtubby",tier:"OU",date:"2016/10/06",battlers:["ABR", "idm tubby"],turns:1287,description:"ABR and Tele *really* hate each other. Like, a lot.",victor:0,elo:2000},
+{filename:"OU-2016-05-10-notdfficult-jskingboo",tier:"ORAS OU",date:"2016/05/10",battlers:["notdfficuLt", "JsKingBoo"],turns:41,description:"Celebrating 1800 Elo featuring a win off hax",victor:1,elo:1800},
+{filename:"OU-2016-05-11-ktutverde-jskingboo",tier:"ORAS OU",date:"2016/05/11",battlers:["Ktutverde", "JsKingBoo"],turns:2,description:"JsKingBoo tilts to oblivion shortly after reaching 1800 Elo",victor:0,elo:1700},
+{filename:"OU-2016-05-14-jskingboo-ultair",tier:"ORAS OU",date:"2016/05/14",battlers:["JsKingBoo", "Ultair"],turns:8,description:"A \"friendly\" match featuring madness and stupidity",victor:0,elo:1500},
+{filename:"OU-2016-09-21-oumodsrfags-noobab",tier:"ORAS OU",date:"2016/09/21",battlers:["OUmodsRfags", "Noobab"],turns:371,description:"Stall versus stall, featuring King Zappy",victor:0,elo:1800},
+{filename:"OU-2016-10-06-abr-idmtubby",tier:"ORAS OU",date:"2016/10/06",battlers:["ABR", "idm tubby"],turns:1287,description:"ABR and Tele *really* hate each other. Like, a lot.",victor:0,elo:2000},
+{filename:"Gen7OU-2017-02-13-jskingboo-mukmuk1",tier:"SM OU",date:"2017/02/13",battlers:["JsKingBoo", "mukmuk1"],turns:322,description:"Mukmuk I hate you",victor:0,elo:1400}
 ];
-
 
 
 
